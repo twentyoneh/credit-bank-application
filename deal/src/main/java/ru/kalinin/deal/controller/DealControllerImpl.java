e41760297ac5513625934c849784bd39ee82c57f
Kalinin Dmitry <didiwot@yandex.ru>
2025-06-18 17:30:44 +0300

    start write controller

--- deal/src/main/java/ru/kalinin/deal/controller/DealControllerImpl.java
+++ deal/src/main/java/ru/kalinin/deal/controller/DealControllerImpl.java
@@ -5,10 +5,8 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import ru.kalinin.common.dto.FinishRegistrationRequestDto;
 import ru.kalinin.common.dto.LoanOfferDto;
 import ru.kalinin.common.dto.LoanStatementRequestDto;
 import ru.kalinin.deal.config.CommonProps;
@@ -22,15 +20,35 @@ import java.util.List;
 @RestController
 @RequestMapping(path = "/deal", produces = MediaType.APPLICATION_JSON_VALUE)
 @RequiredArgsConstructor
-public class DealControllerImpl {
+public class DealControllerImpl implements DealController {
     private final CommonProps commonProps;  // url калькулятора
     private final DealService dealService;
-    private final RestUtil restUtil;    //для общения с другими MS
+//    private final RestUtil restUtil;    //для общения с другими MS
 
+    @Override
     @PostMapping("/statement")
-    public ResponseEntity<List<LoanOfferDto>> createStatement(@RequestBody @Valid LoanStatementRequestDto requestDto){
+    public ResponseEntity<List<LoanOfferDto>> createStatement(
+            @RequestBody @Valid LoanStatementRequestDto requestDto)
+    {
+
         log.info("POST request {} path {}", requestDto, "/deal/statement");
+
         Statement statement = dealService.saveStatement(requestDto);
     }
+
+    @Override
+    @PostMapping("/offer/select")
+    public ResponseEntity<Void> selectStatement(LoanOfferDto requestDto) {
+        return null;
+    }
+
+    @Override
+    @PostMapping("/calculate/{statementId}")
+    public ResponseEntity<Void> finishRegistrationAndCalculateCredit(
+            @PathVariable String statementId,
+            @RequestBody FinishRegistrationRequestDto requestDto) {
+        return null;
+    }
+
 
 }
